b022ea6347cf0495d07984263fecfbae4532f299
Bruno Cabral <bruno.poc@gmail.com>
2020-02-24 16:48:06 -0300

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,19 @@
 module.exports = {
-    mode: process.env.NODE_ENV === "production" ? "production" : "development",
-    output: {
-        libraryTarget: "commonjs2"
-    },
-    resolve: {
-        modules: ['app', 'node_modules'],
-        extensions: [
-          '.js',
-          '.jsx',
-          '.react.js',
-        ],
-        mainFields: [
-          'browser',
-          'jsnext:main',
-          'main',
-        ],
-      },
-      node: { fs: 'empty', net: 'empty' },
-      target: 'web',
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  output: {
+    libraryTarget: "commonjs2"
+  },
+  resolve: {
+    modules: ["app", "node_modules"],
+    extensions: [".js", ".jsx", ".react.js"],
+    mainFields: ["browser", "jsnext:main", "main"]
+  },
+  node: {
+    fs: "empty",
+    net: "empty",
+    child_process: "empty",
+    dns: "empty",
+    tls: "empty"
+  },
+  target: "web"
 };
